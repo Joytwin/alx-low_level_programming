3acd3ee4c550809bcd7aea0eb48b4cec766e15c0
Joytwin <joylineronoh@gmail.com>
2023-06-22 03:19:45 -0700

    10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * ad - Adds to values
+ * add- Adds to values
  * @n1: The first number to be computed
- * %n2: The second nmber to be computed
+ * @n2: The second nmber to be computed
  * Return: Integer value
  */
 int add(int n1, int n2)
