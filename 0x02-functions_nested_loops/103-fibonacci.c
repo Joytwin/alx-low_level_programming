4b24431faabbcfa7c98a195a8199a754e811d8e0
Joytwin <joylineronoh@gmail.com>
2023-07-06 05:13:12 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the sum of the even-valued terms of the fibonacci whose values do not exceed 4,000,000,
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	unsigned long current = 0, next = 1, sum = 0, fin_sum = 0;
+
+	while (1)
+	{
+		sum = current + next;
+		if (sum > 4000000)
+			break;
+		if (sum % 2 == 0)
+			fin_sum += sum;
+		current = next;
+		next = sum;
+	}
+
+	printf("%lu\n", fin_sum);
+
+	return (0);
+}
