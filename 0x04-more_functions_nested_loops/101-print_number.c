c1269813a3f6e20c5beb4fef677b845e294c2cc0
Joytwin <joylineronoh@gmail.com>
2023-07-06 23:30:51 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,26 @@
 #include "main.h"
+
 /**
- * print_number__printsan integer
+ * print_number - prints an integer
  * @n: integer to be printed
  */
-void print_number (int n)
+void print_number(int n)
 {
-	unsigned int 1
-		if (n<0)
-		{
-			n1=-n
-				_putchar ('-');
-		}
-		else
-		{
-			n1=n
-		}
-	if (n1/10)
+	unsigned int n1;
+
+	if (n < 0)
 	{
-		print_number (n1/10);
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
 	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
