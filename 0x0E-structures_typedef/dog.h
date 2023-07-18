6479ed0c62f90481c6222669ef53042a10bea6ca
Joytwin <joylineronoh@gmail.com>
2023-07-17 17:41:21 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+
 #ifndef DOG_H
 #define DOG_H
 /**
@@ -12,15 +13,14 @@ struct dog
 {
 char *name;
 float age;
-char *owner
+char *owner;
 };
+
 
 /**
 * dog_t - typedef for struct dog
-*/
+	 */
 typedef struct dog dog_t;
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
