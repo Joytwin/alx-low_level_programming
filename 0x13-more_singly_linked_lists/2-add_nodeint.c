1da344ace7871a877d0fe40967768bc74b9fa393
Joytwin <joylineronoh@gmail.com>
2023-08-01 15:04:32 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,14 @@
+iinclude "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+
+while (h)
+nnum++;
+h = h->next;
+}
